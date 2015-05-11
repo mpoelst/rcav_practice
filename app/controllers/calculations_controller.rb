96ddf5fb4b902d69b39f93e0d31e48ac61306afd
mpoelst <mpoelstr@chicagobooth.edu>
2015-05-10 20:38:09 -0500

    calculations practice

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
-  def sqrt
+  def square_number
+    @number = params[:number].to_f
+    @square = @number ** 2
+  end
+
+  def squareroot
+    @number = params[:number].to_f
+    @sqrt = Math.sqrt(@number)
   end
 
   def pmt
+    @rate = params[:interest_rate].to_f
+    @payments = params[:number_of_payments].to_f
+    @value = params[:principal_value].to_f
+
+    #rate = @rate / 100 / 12
+    #nper = @payments * 12
+
+    @monthly_payment = ((@rate) * @value)/(1 - (1 + (@rate)** - (@payments)))
   end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/",               { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:number", { :controller => "calculations", :action => "square_number" })
+
+  get("/square_root/:number", { :controller => "calculations", :action => "squareroot" })
+
+  get("/payment/:interest_rate/:number_of_payments/:principal_value", { :controller => "calculations", :action => "pmt" })
 end
+
